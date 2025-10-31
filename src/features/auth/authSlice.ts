9f94cb2b3a90122f12c66983c62be48db1f710aa
Johann Torres <johannccn@gmail.com>
2025-10-30 23:07:41 -0500

    feat: actualizar estilos y agregar layout principal

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -16,7 +16,7 @@ const initialState: AuthState = {
 
 const authSlice = createSlice({
   name: "auth",
-  initialState,
+  initialState: initialState,
   reducers: {},
   extraReducers: (builder) => {
     builder
